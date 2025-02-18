942571edfb06ba74ec4bf0603ef5b0bc255a4b82
Jjini <sojinnuna@gmail.com>
2025-02-18 11:18:01 +0900

    [level 3] Title: 네트워크, Time: 0.15 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,45 +1,28 @@
-import java.util.*;
-
 class Solution {
-    ArrayList<Integer>[] graph;
     boolean[] visited;
+    int answer;
+    
     public int solution(int n, int[][] computers) {
-        graph = new ArrayList[n];
+        answer = 0;
         visited = new boolean[n];
         for(int i=0; i<n; i++){
-            graph[i] = new ArrayList<>();
-        }
-        for(int i=0; i<computers.length-1; i++){
-            for(int j=i+1; j<computers[0].length; j++){
-                int cur = computers[i][j];
-                if(cur == 1){
-                    graph[i].add(j);
-                    graph[j].add(i);
-                }
-            }
-        }
-        int answer = 0;
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                bfs(i, n);
+            if(!visited[i]) {
+                dfs(i, computers);
                 answer++;
             }
         }
         return answer;
     }
     
-    private void bfs(int i, int n){
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(i);
-        visited[i] = true;
+    private void dfs(int cur, int[][] computers) {
+        if(cur == computers.length) {
+            return;
+        }
         
-        while(!q.isEmpty()){
-            int cur = q.poll();
-            for(int a : graph[cur]){
-                if(!visited[a]){
-                    visited[a] = true;
-                    q.add(a);
-                }
+        for(int i=0; i<computers.length; i++) {
+            if(!visited[i] && computers[cur][i] == 1) {
+                visited[i] = true;
+                dfs(i, computers);
             }
         }
     }
