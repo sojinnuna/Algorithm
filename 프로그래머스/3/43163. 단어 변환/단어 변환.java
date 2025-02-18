17ea0ee82deb8564e114dd08ec00c57270f8a00b
Jjini <sojinnuna@gmail.com>
2025-02-18 10:41:37 +0900

    [level 3] Title: 단어 변환, Time: 0.32 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    boolean[] visited;
+    int answer;
+    
+    public int solution(String begin, String target, String[] words) {
+        visited = new boolean[words.length];
+        dfs(begin, target, words, 0);
+        return answer;
+    }
+    
+    private void dfs(String begin, String target, String[] words, int count) {
+        //만약 같으면 리턴
+        if(begin.equals(target)) {
+            answer = count;
+            return;
+        }
+
+        //for문
+        for(int i=0; i<words.length; i++) {
+            if(!visited[i]) {
+                int k = 0;
+                String cur = words[i];
+            
+                for(int j=0; j<cur.length(); j++){
+                    if(begin.charAt(j) == cur.charAt(j)) {
+                        k++;
+                    }   
+                }
+                
+                if(k==begin.length()-1) {
+                    visited[i] = true;
+                    dfs(words[i], target, words, count+1);
+                    visited[i] = false;
+                }
+            }
+        }
+        
+    }
+}
