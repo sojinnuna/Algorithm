484e6b10077d51dd7b8b6b88e797d0dd89a20539
Jjini <sojinnuna@gmail.com>
2025-02-23 19:22:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.69 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,46 +4,43 @@ class Solution {
     int[] dx = {-1, 1, 0, 0};
     int[] dy = {0, 0, -1, 1};
     boolean[][] visited;
+    int answer;
     
     public int solution(int[][] maps) {
+        answer = 0;
         visited = new boolean[maps.length][maps[0].length];
-        
-        int answer= bfs(maps);
-        if(answer == 0) {
-            return -1;
-        } else {
-            return answer;
-        }
+        bfs(maps);
+        if(answer == 0) return -1;
+        return answer;
     }
     
-    private int bfs(int[][] maps) {
+    private void bfs(int[][] maps) {
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{0, 0, 1});
         visited[0][0] = true;
-        int answer = 0;
         
-        while(!q.isEmpty()){
+        while(!q.isEmpty()) {
             int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
+            int a = cur[0];
+            int b = cur[1];
             int len = cur[2];
             
-            if(x == maps.length-1 && y == maps[0].length-1) {
+            if(a == maps.length-1 && b == maps[0].length-1) {
                 answer = len;
                 break;
             }
             
             for(int i=0; i<4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(nx>=0 && nx<maps.length && ny>=0 && ny<maps[0].length) {
-                    if(!visited[nx][ny] && maps[nx][ny] == 1) {
-                        visited[nx][ny] = true;
-                        q.add(new int[]{nx, ny, len+1});
+                int na = a+dx[i];
+                int nb = b+dy[i];
+                
+                if(na>=0 && na<maps.length && nb>=0 && nb<maps[0].length) {
+                    if(!visited[na][nb] && maps[na][nb] == 1) {
+                        visited[na][nb] = true;
+                        q.add(new int[]{na, nb, len+1});
                     }
                 }
             }
         }
-        return answer;
     }
 }
