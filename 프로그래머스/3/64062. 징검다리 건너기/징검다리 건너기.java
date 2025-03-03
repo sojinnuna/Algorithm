7c571f70f2702ea7fab30d59f36518925cd7439e
Jjini <sojinnuna@gmail.com>
2025-03-03 11:39:55 +0900

    [level 3] Title: 징검다리 건너기, Time: 24.98 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,29 +1,26 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] stones, int k) {
         int answer = 0;
         
         int min = 0;
         int max = 200000000;
-        int count = 0;
         while(min <= max) {
             int mid = (min+max)/2;
-            count = 0;
-
-            if(check(stones, mid, 0, k)) {
+            if(check(stones, k, mid)) {
                 answer = Math.max(answer, mid);
                 min = mid+1;
             } else {
                 max = mid-1;
             }
         }
+        
         return answer;
     }
     
-    private boolean check(int[] stones, int cur, int count, int k) {
-        for(int s : stones) {
-            if(s < cur) {
+    private boolean check(int[] stones, int k, int cur) {
+        int count = 0;
+        for(int i : stones) {
+            if(i < cur) {
                 count++;
             } else {
                 count = 0;
@@ -33,7 +30,7 @@ class Solution {
                 return false;
             }
         }
+        
         return true;
     }
 }
-    
