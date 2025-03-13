2b5236b86cea05ef3c4c7bf547f602de3a001407
Jjini <sojinnuna@gmail.com>
2025-03-13 10:27:55 +0900

    [level 2] Title: 모음 사전, Time: 13.88 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,15 +1,16 @@
 import java.util.*;
 
 class Solution {
+    String[] alp = {"A", "E", "I", "O", "U"};
     ArrayList<String> list = new ArrayList<>();
-    String[] s = {"A", "E", "I", "O", "U"};
     
     public int solution(String word) {
         int answer = 0;
         dfs("");
+        
         for(int i=0; i<list.size(); i++) {
             if(list.get(i).equals(word)) {
-                answer = i;
+                answer = i+1;
                 break;
             }
         }
@@ -17,16 +18,21 @@ class Solution {
         return answer;
     }
     
-    private void dfs(String cur) {
-        //예외 처리
-        list.add(cur);
-        
-        if(cur.length() == 5) {
+    private void dfs(String cur) { 
+        if(cur.length() > 5) {
             return;
         }
         
-        for(int i=0; i<s.length; i++) {
-            dfs(cur+s[i]);
+        if(cur.length() > 0) {
+            list.add(cur);
+        }
+        
+        
+        for(int i=0; i<alp.length; i++) {
+            cur += alp[i];
+            dfs(cur);
+            cur = cur.substring(0, cur.length()-1);
+
         }
     }
 }
