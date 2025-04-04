41ca4d7a400f97a1176cdc97f24ae1615437c146
Jjini <sojinnuna@gmail.com>
2025-04-04 14:00:54 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 12.80 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,56 +1,61 @@
 import java.util.*;
 
 class Solution {
+    int[][] map;
+    ArrayList<Integer> list = new ArrayList<>();
+    
     public int[] solution(int rows, int columns, int[][] queries) {
-        int[][] box = new int[rows][columns];
-        int[] answer = new int[queries.length];
-        
-        // 2차원 배열 초기화
-        for (int i = 0, num = 1; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                box[i][j] = num++;
+        map = new int[rows][columns];
+        int index = 1;
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<columns; j++) {
+                map[i][j] = index++;
             }
         }
         
-        for (int q = 0; q < queries.length; q++) {
-            int x1 = queries[q][0] - 1;
-            int y1 = queries[q][1] - 1;
-            int x2 = queries[q][2] - 1;
-            int y2 = queries[q][3] - 1;
+        for(int i=0; i<queries.length; i++) {
+            int sx = queries[i][0]-1;
+            int ex = queries[i][2]-1;
             
-            int temp = box[x1][y1]; // 첫 번째 값 임시 저장
-            int minVal = temp;
-
-            // ← 왼쪽 (위쪽 가로줄)
-            for (int i = x1; i < x2; i++) {
-                box[i][y1] = box[i + 1][y1];
-                minVal = Math.min(minVal, box[i][y1]);
-            }
-
-            // ↓ 아래쪽 (오른쪽 세로줄)
-            for (int i = y1; i < y2; i++) {
-                box[x2][i] = box[x2][i + 1];
-                minVal = Math.min(minVal, box[x2][i]);
-            }
-
-            // → 오른쪽 (아래쪽 가로줄)
-            for (int i = x2; i > x1; i--) {
-                box[i][y2] = box[i - 1][y2];
-                minVal = Math.min(minVal, box[i][y2]);
-            }
-
-            // ↑ 위쪽 (왼쪽 세로줄)
-            for (int i = y2; i > y1 + 1; i--) {
-                box[x1][i] = box[x1][i - 1];
-                minVal = Math.min(minVal, box[x1][i]);
-            }
-
-            // 마지막 값 보정
-            box[x1][y1 + 1] = temp;
-
-            answer[q] = minVal;
+            int sy = queries[i][1]-1;
+            int ey = queries[i][3]-1;
+            
+            dfs(sx, ex, sy, ey);
         }
         
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
         return answer;
+    }
+    
+    private void dfs(int sx, int ex, int sy, int ey) {
+        int cur = map[sx][sy];
+        int min = cur;
+        
+        for(int i=sx; i<ex; i++) {
+            map[i][sy] = map[i+1][sy];
+            min = Math.min(min, map[i][sy]);
+        }
+        
+        for(int i=sy; i<ey; i++) {
+            map[ex][i] = map[ex][i+1];
+            min = Math.min(min, map[ex][i]);
+        }
+        
+        for(int i=ex; i>sx; i--) {
+            map[i][ey] = map[i-1][ey];
+            min = Math.min(min, map[i][ey]);
+        }
+        
+        for(int i=ey; i>sy; i--) {
+            map[sx][i] = map[sx][i-1];
+            min = Math.min(min, map[sx][i]);
+        }
+        
+        map[sx][sy+1] = cur;
+        list.add(min);
     }
 }
